4266d7f91eae3cef9a4bee624ae82df1aefccb83
sidbro_archpc <sasha.sidenko09@gmail.com>
2025-11-11 23:02:00 +0300

    updated migrate files

--- migrations/000002_create_tasks_table.down.sql
+++ migrations/000002_create_tasks_table.down.sql
@@ -1,1 +1,4 @@
-DROP TABLE tasks;
+DROP TRIGGER IF EXISTS trg_tasks_update_timestamp ON tasks;
+DROP TABLE IF EXISTS tasks;
+
+DROP FUNCTION IF EXISTS _update_timestamp()
--- migrations/000002_create_tasks_table.up.sql
+++ migrations/000002_create_tasks_table.up.sql
@@ -1,8 +1,24 @@
-CREATE TABLE tasks {
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     title VARCHAR(150) NOT NULL,
     description TEXT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-};
+);
+
+CREATE INDEX IF NOT EXISTS idx_tasks_user_id ON tasks(user_id);
+
+CREATE OR REPLACE FUNCTION _update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = now();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql VOLATILE;
+
+DROP TRIGGER IF EXISTS trg_tasks_update_timestamp ON tasks;
+CREATE TRIGGER trg_tasks_update_timestamp
+BEFORE UPDATE ON tasks
+FOR EACH ROW
+EXECUTE FUNCTION _update_timestamp();
