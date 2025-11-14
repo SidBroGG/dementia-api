4a64ec6e17fe6d14c89d234b21c5f0aed145e784
sidbro_archpc <sasha.sidenko09@gmail.com>
2025-11-14 13:08:25 +0300

    fixed db

--- internal/store/postgres.go
+++ internal/store/postgres.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/SidBroGG/dementia-api/config"
 	"github.com/jmoiron/sqlx"
@@ -12,6 +13,8 @@ import (
 func NewPostgresDB(cfg config.DB) (*sqlx.DB, error) {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Name)
+
+	log.Println("Connecting with dsn:", dsn)
 
 	db, err := sqlx.Connect("postgres", dsn)
 	if err != nil {
